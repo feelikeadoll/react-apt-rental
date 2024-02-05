571644ebfa1349da4378b2e209ae9caaff764532
Laia Navalon <laianavalon@gmail.com>
2024-02-05 17:41:11 +0100

    Style css

--- react-apt-rental/src/components/Navbar.jsx
+++ react-apt-rental/src/components/Navbar.jsx
@@ -1,10 +1,16 @@
 import logo from "../assets/logo.png"
+import menu from "../assets/menu.png"
 
 function Navbar(props) {
     return (
         <div id="navbar">
-            <h1>{props.name}</h1>
-            <img src={logo} alt={"logo"} />
+            <div class="logo">
+                <img src={logo} alt={"logo"} id="logo-img"/>
+                <h1>{props.name}</h1>
+            </div>
+            <div>
+                <img src={menu} alt="menu-icon" id="menu-icon"/>
+            </div>
         </div>
     )
 }
