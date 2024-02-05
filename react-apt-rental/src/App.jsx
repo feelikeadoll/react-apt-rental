20a7fcf01772380e16cfbdd6efdefe87ee3cde9b
Laia Navalon <laianavalon@gmail.com>
2024-02-05 16:55:40 +0100

    Add Footer + Sidebar components

--- react-apt-rental/src/App.jsx
+++ react-apt-rental/src/App.jsx
@@ -1,4 +1,6 @@
 import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import Sidebar from "./components/Sidebar"
 import './App.css'
 
 function App() {
@@ -6,6 +8,8 @@ function App() {
   return (
     <>
       <Navbar name="Home Sweet Home" />
+      <Sidebar></Sidebar>
+      <Footer />
     </>
   )
 }
--- react-apt-rental/src/components/Navbar.jsx
+++ react-apt-rental/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/logo.png"
 
 function Navbar(props) {
     return (
-        <div>
+        <div id="navbar">
             <h1>{props.name}</h1>
             <img src={logo} alt={"logo"} />
         </div>
