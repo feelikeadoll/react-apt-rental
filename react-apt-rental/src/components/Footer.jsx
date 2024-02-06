f5f01b851f086c0136ed650bb03fe220698291cd
erik busch <eazyErik>
2024-02-06 18:31:30 +0100

    create list and list item

--- react-apt-rental/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <div id="footer">
-            <p><a href="https://github.com/feelikeadoll/react-apt-rental">Home Sweet Home</a> built by Erik Busch and Laia Navalón.</p>
-        </div>
-    )
-}
-
-export default Footer;
