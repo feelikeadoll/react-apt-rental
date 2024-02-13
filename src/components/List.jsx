2a9efee7ae7201d03f280463996c1c06e46182c0
Laia Navalon <laianavalon@gmail.com>
2024-02-13 16:07:31 +0100

    Implement favourites feature

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,12 +1,24 @@
 import { ListItem } from "./ListItem";
+import { useState } from "react";
 
 export const List = (props) => {
+
+  const [favourites, setFavourites] = useState([])
+
+  function handleAddFavourites(apartmentId) {
+    console.log(apartmentId)
+    const newList = [apartmentId, ...favourites];
+    setFavourites(newList)
+  }
+  console.log(favourites)
+
   let appartmentsList = props.appartments.map((element) => {
     return (
       <ListItem
         key={element.id}
         appartment={element}
         function={props.deleteAppartment}
+        handleAddFavourites={handleAddFavourites}
       />
     );
   });
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,23 +1,30 @@
 import { Link } from "react-router-dom";
+import bookmark from "../assets/bookmark-1.png"
+
 
 export const ListItem = (props) => {
   const handleDeleteButton = () => {
     return props.function(props.appartment.id);
   };
 
+  const currentApt = props.appartment;
+  
   return (
     <div className="card">
       <h3>
         {props.appartment.property_type} in {props.appartment.city},{" "}
         {props.appartment.country}
       </h3>
-      <Link to={`/apartments/${props.appartment.id}`}>
-        <img
-          className="picture"
-          src={props.appartment.picture_url.url}
-          alt=""
-        />
-      </Link>
+      <div className="imgFrame">
+        <button onClick={() => {props.handleAddFavourites(currentApt)}} className="menu-btn"><img src={bookmark} className="bookmark-btn" /></button>
+        <Link to={`/apartments/${props.appartment.id}`}>
+          <img
+            className="picture"
+            src={props.appartment.picture_url.url}
+            alt=""
+          />
+        </Link>
+      </div>
       <div className="apt-info">
         <p>Capacity: {props.appartment.accommodates}</p>
         <p>Price: {props.appartment.price}€</p>
